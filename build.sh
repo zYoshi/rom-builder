736a6896442111e399c5b48b2534d876934a8aa0
Ad!thya R <adithya.r02@outlook.com>
2019-07-27 23:25:32 +0530

    rom_builder: Improve the main build script
    * Some misc code improvements.

--- build.sh
+++ build.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
+#
+# buildbot script for compiling android ROMs using drone CI
 
 source /drone/src/config.sh
 
-# Email for git
-ci_repo=$(cat /drone/src/.git/config | grep url | sed 's|url = https://github.com/||' | sed 's|.git||')
+ci_repo=$(cat /drone/src/.git/config | grep url | sed -e 's|url = https://github.com/||' -e 's|.git||')
+ci_url=$(echo "https://cloud.drone.io/$ci_repo/$(cat /tmp/build_no)/1/2" | tr -d " ")
+
+# Configure git
 git config --global user.email "$GITHUB_EMAIL"
 git config --global user.name "$GITHUB_USER"
 
-TELEGRAM_TOKEN=$(cat /tmp/tg_token)
-TELEGRAM_CHAT=$(cat /tmp/tg_chat)
-GITHUB_TOKEN=$(cat /tmp/gh_token)
+export TELEGRAM_TOKEN=$(cat /tmp/tg_token)
+export TELEGRAM_CHAT=$(cat /tmp/tg_chat)
+export GITHUB_TOKEN=$(cat /tmp/gh_token)
 
-export TELEGRAM_TOKEN
-export TELEGRAM_CHAT
-export GITHUB_TOKEN
-
-function trim_darwin() {
+# Delete useless darwin repos
+trim_darwin() {
     cd .repo/manifests
-    cat default.xml | grep -v darwin  >temp  && cat temp >default.xml  && rm temp
+	sed -i '/darwin/d' default.xml
     git commit -a -m "Magic"
     cd ../
-    cat manifest.xml | grep -v darwin  >temp  && cat temp >manifest.xml  && rm temp
+	sed -i '/darwin/d' manifest.xml
     cd ../
 }
 
-export outdir="out/target/product/$device"
-ci_url="$(echo "https://cloud.drone.io/"$ci_repo"/"$(cat /tmp/build_no)"/1/2" | tr -d " ")"
-
 cd /home/ci
-
-git clone https://"$GITHUB_USER":"${GITHUB_TOKEN}"@github.com/"$GITHUB_USER"/google-git-cookies.git >/dev/null  2>&1
+git clone -q "https://$GITHUB_USER:${GITHUB_TOKEN}@github.com/$GITHUB_USER/google-git-cookies.git" &> /dev/null
 if [ -e google-git-cookies ]; then
-    cd google-git-cookies
-    bash setup_cookies.sh
-    cd ../
+    bash google-git-cookies/setup_cookies.sh
     rm -rf google-git-cookies
 else
     echo "google-git-cookies repo not found on your account, see steps on README"
@@ -42,59 +37,58 @@ fi
 mkdir "$ROM"
 cd "$ROM"
 
-repo init -u "$manifest_url" -b "$branch" --depth 1 >/dev/null  2>&1
-echo "Sync started for "$manifest_url""
-telegram -M "Sync Started for ["$ROM"]("$manifest_url")"
-SYNC_START=$(date +"%s")
-trim_darwin >/dev/null   2>&1
-repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q 2>&1 >>logwe 2>&1
-bash /drone/src/clone.sh
-SYNC_END=$(date +"%s")
-SYNC_DIFF=$((SYNC_END - SYNC_START))
-if [ -e frameworks/base ]; then
-    echo "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+export outdir="out/target/product/$device"
+
+# Initialize repo
+repo init -u "$manifest_url" -b "$branch" --depth 1 &> /dev/null
+trim_darwin &> /dev/null
+echo "Sync started for $manifest_url"
+telegram -M "Sync Started for [$ROM]($manifest_url)"
+
+# Reset bash timer and begin syncing
+SECONDS=0
+if repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q &> /dev/null; then
+	# Syncing completed, clone custom repos if any
+	bash /drone/src/clone.sh
+
+	echo "Sync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
     echo "Build Started"
-    telegram -M "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds
+    telegram -M "Sync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
 
-Build Started: [See Progress]("$ci_url")"
+Build Started: [See Progress]($ci_url)"
+	
+	# Reset bash timer and begin compilation
+    SECONDS=0
+    source build/envsetup.sh &> /dev/null
+    if [ -e "device/$oem/$device" ]; then python3 /drone/src/dependency_cloner.py; fi
+    lunch $rom_vendor_name_$device-userdebug &> /dev/null
 
-    BUILD_START=$(date +"%s")
-
-    . build/envsetup.sh >/dev/null  2>&1
-    source /drone/src/config.sh
-    if [ -e device/"$oem"/"$device" ]; then
-        python3 /drone/src/dependency_cloner.py
-    fi
-    lunch "$rom_vendor_name"_"$device"-userdebug >/dev/null  2>&1
-    mka bacon | grep "$device"
-    BUILD_END=$(date +"%s")
-    BUILD_DIFF=$((BUILD_END - BUILD_START))
-
-    export finalzip_path=$(ls "$outdir"/*201*.zip | tail -n -1)
-    export zip_name=$(echo "$finalzip_path" | sed "s|"$outdir"/||")
-    export tag=$( echo "$zip_name" | sed 's|.zip||')
-    if [ -e "$finalzip_path" ]; then
-        echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-
+    if mka bacon | grep $device; then
+		# Build completed succesfully, upload it to github
+		export finalzip_path=$(ls "$outdir/*2019*.zip" | tail -n -1)
+		export zip_name=$(basename -s "$finalzip_path")
+        echo "Build completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
         echo "Uploading"
 
-        github-release "$release_repo" "$tag" "master" ""$ROM" for "$device"
+        github-release "$release_repo" "$zip_name" "master" "$ROM for $device
 
 Date: $(env TZ="$timezone" date)" "$finalzip_path"
 
         echo "Uploaded"
 
-        telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
+        telegram -M "Build completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
 
-Download: ["$zip_name"](https://github.com/"$release_repo"/releases/download/"$tag"/"$zip_name")"
+Download: [$zip_name](https://github.com/$release_repo/releases/download/$zip_name/"$zip_name".zip)"
 
     else
-        echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-        telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
+		# Build failed
+        echo "ALERT: Build failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+        telegram -N -M "ALERT: Build failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
         exit 1
     fi
 else
-    echo "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
-    telegram -N -M "Sync failed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+	# Sync failed
+    echo "ALERT: Sync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+    telegram -N -M "ALERT: Sync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
     exit 1
 fi
